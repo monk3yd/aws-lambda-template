6e42b75a9f48e36cc9c9527887d5031a3368ebb6
Juan Gajardo <jegajardog@gmail.com>
2023-08-15 02:39:22 -0300

    Remove experiment push to ECR

--- scripts/deploy-images-to-ecr.sh
+++ scripts/deploy-images-to-ecr.sh
@@ -26,19 +26,19 @@ echo ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com/${IMAGE_NAME}:${IMAGE_TA
 
 
 ## Experimental image
-IMAGE_TAG="experimental"
+# IMAGE_TAG="experimental"
 
 # Login
-aws ecr get-login-password --region ${REGION_NAME} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com
+# aws ecr get-login-password --region ${REGION_NAME} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com
 
 # Build docker image
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f docker/Dockerfile .
+# docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f docker/Dockerfile .
 
 # Tag docker image to ECR (map)
-docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com/${IMAGE_NAME}:${IMAGE_TAG}
+# docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com/${IMAGE_NAME}:${IMAGE_TAG}
 
 # Push docker image to ECR
-docker push ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com/${IMAGE_NAME}:${IMAGE_TAG}
+# docker push ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com/${IMAGE_NAME}:${IMAGE_TAG}
 
-echo ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com/${IMAGE_NAME}:${IMAGE_TAG} > scripts/data/ecr_repo_${IMAGE_TAG}.txt
+# echo ${ACCOUNT_ID}.dkr.ecr.${REGION_NAME}.amazonaws.com/${IMAGE_NAME}:${IMAGE_TAG} > scripts/data/ecr_repo_${IMAGE_TAG}.txt
 
