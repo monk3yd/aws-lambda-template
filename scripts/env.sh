2e516b9e8105eea6c660b33aa78b2eecbb920fa4
Juan Gajardo <jegajardog@gmail.com>
2023-10-21 19:50:11 -0300

    Add env.sh script, need to source it

---
+++ scripts/env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Need to source this script for env variables to be available outside the context of the script
+# command: source env.sh
+
+export PROJECT_NAME="project-name"
+echo "PROJECT_NAME is $PROJECT_NAME"
+
+export AWS_ACCOUNT_ID="164430786407"
+echo "AWS_ACCOUNT_ID is $AWS_ACCOUNT_ID"
+
+export AWS_ACCESS_KEY_ID="AKIA1AVBVN4JQP6VNQ7K"
+echo "AWS_ACCESS_KEY_ID is $AWS_ACCESS_KEY_ID"
+
+export AWS_SECRET_ACCESS_KEY="fa89eADADI1hXHPgL15Y0eB2inHMVu3o0w5U8B8F"
+echo "AWS_SECRET_ACCESS_KEY is $AWS_SECRET_ACCESS_KEY" 
+
+export AWS_REGION_NAME="us-east-1"
+echo "AWS_REGION_NAME is $AWS_REGION_NAME"
+
+# export AWS_PROFILE=""
+# echo "AWS_PROFILE is $AWS_PROFILE"
+
